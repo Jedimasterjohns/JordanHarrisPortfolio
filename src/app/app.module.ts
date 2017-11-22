f4ed08f7bb18bf53cf0f414d73a519f94eef571f
Ryan Johns <I59227@xactware.com>
2017-11-22 12:25:41 -0700

    Having trouble getting app to compile after adding angular materials. Looking at verison on package json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+import { routing } from './app.routing';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav/index';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes, } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSidenavModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
