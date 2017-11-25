4ff8ad5bcf58ec2d70c12f9c63041294be168b0a
Ryan <jedimasterjohns@gmail,com>
2017-11-24 19:19:26 -0700

    Still working on sidenav functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { routing } from './app.routing';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes, } from '@angular/router';
+import { AppNavigationComponent } from './app-navigation/app-navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AppNavigationComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
     routing
   ],
   providers: [],
