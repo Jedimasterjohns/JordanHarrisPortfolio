0d13a0e9f8391999c6e39df83db7fb6cd99693d0
Ryan Johns <I59227@xactware.com>
2017-12-01 15:14:34 -0700

    whatevs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
@@ -6,70 +6,11 @@ import { MatSidenavModule } from '@angular/material/sidenav';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    // google npm typewriting and use that to write cursor greeting lines
   }
-
-  var greetingLine1 = "Software developer with a tenacity to increase efficiency and inspire creativity.";
-  var greetingLine2 = "Jr Developer at Zamplebox specializing in web and desktop test automation.";
-  var greetingLine3 = "I build <em>sturdy testing frameworks</em> and <em>responsive web applications</em>.";
-
-  $(document).ready(function() {
-      $(document).foundation();
-      new FastClick(document.body);
-
-      $(".scroll-prompter").hide();
-
-      $(".scroll-prompter a").click(function() {
-          $(this).fadeOut(200);
-          $.scrollTo(".skill-logos", {duration: 400});
-          $(window).scroll(function() {
-              if ($(this).scrollTop() == 0)
-                  $(".scroll-prompter a").fadeIn(400);
-          });
-      });
-
-      $(".intro-greeting").hide().slideDown('slow', function() {
-          $(".intro-text-1").typed({
-              strings: ["^1500" + greetingLine1 + "^1800"],
-              typeSpeed: 0,
-              cursorChar: " |",
-              callback: function() {
-                  $(".typed-cursor").fadeOut("fast");
-                  $(".intro-text-2").typed({
-                      strings: [greetingLine2 + "^1800<br>"],
-                      typeSpeed: 0,
-                      cursorChar: " |",
-                      callback: function() {
-                          $(".typed-cursor").fadeOut("fast");
-                          $(".intro-text-3").typed({
-                              strings: [greetingLine3],
-                              typeSpeed: 0,
-                              cursorChar: " |",
-                              callback: function() {
-                                  $(".scroll-prompter").fadeIn("slow");
-                              }
-                          });
-                      }
-                  });
-              }
-          });
-      });
-
-      $(".site-intro").click(function(e) {
-          // Hides all three lines and breaks in one go
-          $(".intro-text-1").parent().hide();
-
-          $(".intro-text-1-skipped").html(greetingLine1);
-          $(".intro-text-2-skipped").html(greetingLine2);
-          $(".intro-text-3-skipped").html(greetingLine3);
-
-          $(".scroll-prompter").fadeIn("slow");
-          $(".typed-cursor").hide();
-      });
-  });
-
 }
