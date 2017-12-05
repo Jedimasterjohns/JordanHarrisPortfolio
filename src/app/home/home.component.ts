8b753c19afdc8b75290cf81b2b53a0a9aa9b81cc
Ryan Johns <I59227@xactware.com>
2017-12-05 14:26:39 -0700

    Progress with typerwriter effect using TypedJS. All that is left is styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,47 +1,32 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import * as $ from 'jquery';
+import * as Typed from 'typed.js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 
 export class HomeComponent implements AfterViewInit {
+  public get typedCursor(): Element {
+    return document.querySelectorAll('.typed-cursor')[0];
+  }
   constructor() { }
 
   ngAfterViewInit() {
-
-    const greetingLine1 = 'Software developer with a tenacity to increase efficiency and inspire creativity.';
-    const greetingLine2 = 'Jr Developer at Zamplebox specializing in web and desktop test automation.';
-    const greetingLine3 = 'I build <em>sturdy testing frameworks</em> and <em>responsive web applications</em>.';
-
-    $('.intro-greeting').hide().slideDown('slow', function() {
-      $('.intro-text-1').typed({
-          strings: ['^1500' + greetingLine1 + '^1800'],
-          typeSpeed: 0,
-          cursorChar: ' |',
-          callback: function() {
-              $('.typed-cursor').fadeOut('fast');
-              $('.intro-text-2').typed({
-                  strings: [greetingLine2 + '^1800<br>'],
-                  typeSpeed: 0,
-                  cursorChar: ' |',
-                  callback: function() {
-                      $('.typed-cursor').fadeOut('fast');
-                      $('.intro-text-3').typed({
-                          strings: [greetingLine3],
-                          typeSpeed: 0,
-                          cursorChar: ' |',
-                          callback: function() {
-                              $('.scroll-prompter').fadeIn('slow');
-                          }
-                      });
-                  }
-              });
-          }
-      });
-  });
+    const greetingLine = 'Software developer with a tenacity to increase efficiency and inspire creativity.<br>' +
+                          'Jr Developer at Zamplebox specializing in web and desktop test automation.<br><br>' +
+                          'I build <em>sturdy testing frameworks</em> and <em>responsive web applications</em>.';
+    let typed = new Typed('.intro-text.intro-text-1', {
+      strings: [greetingLine],
+      startDelay: 800,
+      typeSpeed: 25,
+      cursorChar: ' |',
+      callback: function() {
+        let typedCursor = document.querySelectorAll('.typed-cursor')[0] as HTMLElement;
+        typedCursor.style.display = 'none';
+      }
+    });
   }
 }
