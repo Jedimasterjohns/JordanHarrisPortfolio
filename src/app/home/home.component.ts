875eb8ba7e2a1d52ba511cfae7b27d79fa010709
Ryan Johns <I59227@xactware.com>
2017-12-05 15:01:26 -0700

    Finally got TypedJS to work correctly and typewriter effect text is working correctly

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,15 +18,13 @@ export class HomeComponent implements AfterViewInit {
     const greetingLine = 'Software developer with a tenacity to increase efficiency and inspire creativity.<br>' +
                           'Jr Developer at Zamplebox specializing in web and desktop test automation.<br><br>' +
                           'I build <em>sturdy testing frameworks</em> and <em>responsive web applications</em>.';
-    let typed = new Typed('.intro-text.intro-text-1', {
+    let typed = new Typed('.intro-text', {
       strings: [greetingLine],
-      startDelay: 800,
+      startDelay: 1500,
       typeSpeed: 25,
+      autoInsertCss: true,
       cursorChar: ' |',
-      callback: function() {
-        let typedCursor = document.querySelectorAll('.typed-cursor')[0] as HTMLElement;
-        typedCursor.style.display = 'none';
-      }
+      fadeOut: true
     });
   }
 }
