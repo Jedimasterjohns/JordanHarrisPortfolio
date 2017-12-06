f8fd48d45077627c8487a50c441a8c0a1812d20a
Ryan <jedimasterjohns@gmail,com>
2017-12-05 19:07:50 -0700

    Fixed messagin for typed js phrases

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,15 +9,12 @@ import * as Typed from 'typed.js';
 })
 
 export class HomeComponent implements AfterViewInit {
-  public get typedCursor(): Element {
-    return document.querySelectorAll('.typed-cursor')[0];
-  }
   constructor() { }
 
   ngAfterViewInit() {
-    const greetingLine = 'Software developer with a tenacity to increase efficiency and inspire creativity.<br>' +
-                          'Jr Developer at Zamplebox specializing in web and desktop test automation.<br><br>' +
-                          'I build <em>sturdy testing frameworks</em> and <em>responsive web applications</em>.';
+    const greetingLine =  'UX/UI Designer with a voracious curiosity and a tenacity for creativity.<br>' +
+                          'Recent graduate seeking opportunities in UX/UI software development and web design.<br><br>' +
+                          'I create <em>simple workflows</em> and <em>satisfying user experiences</em>.';
     let typed = new Typed('.intro-text', {
       strings: [greetingLine],
       startDelay: 1500,
